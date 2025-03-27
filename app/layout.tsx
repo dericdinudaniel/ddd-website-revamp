ce0df7df1d8d8511cd1ebb2a63f54fd1af372161
Deric Dinu Daniel <dericdd@umich.edu>
2025-03-27 11:44:59 -0400

    got the sticky scroll to work finally

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="no-scrollbar" suppressHydrationWarning>
       {/* <head>
         <script
           crossOrigin="anonymous"
--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -4,33 +4,43 @@ import TopArtists from "@/components/spotify/TopArtists";
 import Experience from "@/components/Experience";
 import AboutMe from "@/components/AboutMe";
 
-const Page1 = () => {
+type SectionProps = {
+  className?: string;
+};
+
+const Section1 = ({ className = "" }: SectionProps) => {
   return (
-    <div className="min-h-screen w-screen flex flex-col justify-center items-center text-center">
+    <section
+      className={`${className} min-h-screen flex flex-col justify-center items-center text-center sticky top-0 bg-background`}
+    >
       <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-9xl font-bold font-header tracking-[.1rem]">
         Deric Dinu Daniel
       </h1>
       <p className="text-xs sm:text-base md:text-lg lg:text-xl xl:text-2xl mt-1">
         Software Engineer. Music Producer.
       </p>
-    </div>
+    </section>
   );
 };
 
-const Page2 = () => {
+const Section2 = ({ className = "" }: SectionProps) => {
   return (
-    <div className="min-h-screen w-screen flex items-center justify-center px-5 border border-border">
+    <section
+      className={`${className} min-h-screen flex flex-col justify-center items-center text-center sticky top-0 bg-background border-t border-border`}
+    >
       <div className="flex flex-col w-full md:flex-row justify-center gap-x-10 gap-y-6">
         <Experience />
         <AboutMe />
       </div>
-    </div>
+    </section>
   );
 };
 
-const Page3 = () => {
+const Section3 = ({ className = "" }: SectionProps) => {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center">
+    <section
+      className={`${className} min-h-screen flex flex-col justify-center items-center text-center sticky top-0 bg-background border-t border-border`}
+    >
       <div className="flex flex-col sm:flex-row sm:justify-center sm:gap-x-6 w-[100%] gap-y-6 px-10">
         <div className="w-full sm:w-1/2 max-w-[600px] flex justify-center">
           <TopTracks />
@@ -39,19 +49,18 @@ const Page3 = () => {
           <TopArtists />
         </div>
       </div>
-    </div>
+      {/* create space for footer at the bottom for sm and smaller */}
+      <div className="h-20 sm:hidden"></div>
+    </section>
   );
 };
 
 export default function MainContent() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-start w-full overflow-auto">
-      <Page1 />
-      <Page2 />
-      <Page3 />
-
-      {/* create space for footer at the bottom for sm and smaller */}
-      <div className="h-20 sm:hidden"></div>
+    <main className="min-h-[300vh]">
+      <Section1 />
+      <Section2 />
+      <Section3 />
     </main>
   );
 }
