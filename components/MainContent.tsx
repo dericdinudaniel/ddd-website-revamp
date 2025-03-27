efa5a1599af16655f7ee2dee21fe18140dbc781f
Deric Dinu Daniel <dericdd@umich.edu>
2025-03-27 11:46:13 -0400

    removed accidental text-center

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -11,7 +11,7 @@ type SectionProps = {
 const Section1 = ({ className = "" }: SectionProps) => {
   return (
     <section
-      className={`${className} min-h-screen flex flex-col justify-center items-center text-center sticky top-0 bg-background`}
+      className={`${className} min-h-screen flex flex-col justify-center items-center sticky top-0 bg-background`}
     >
       <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-9xl font-bold font-header tracking-[.1rem]">
         Deric Dinu Daniel
@@ -26,7 +26,7 @@ const Section1 = ({ className = "" }: SectionProps) => {
 const Section2 = ({ className = "" }: SectionProps) => {
   return (
     <section
-      className={`${className} min-h-screen flex flex-col justify-center items-center text-center sticky top-0 bg-background border-t border-border`}
+      className={`${className} min-h-screen flex flex-col justify-center items-center sticky top-0 bg-background border-t border-border`}
     >
       <div className="flex flex-col w-full md:flex-row justify-center gap-x-10 gap-y-6">
         <Experience />
@@ -39,7 +39,7 @@ const Section2 = ({ className = "" }: SectionProps) => {
 const Section3 = ({ className = "" }: SectionProps) => {
   return (
     <section
-      className={`${className} min-h-screen flex flex-col justify-center items-center text-center sticky top-0 bg-background border-t border-border`}
+      className={`${className} min-h-screen flex flex-col justify-center items-center sticky top-0 bg-background border-t border-border`}
     >
       <div className="flex flex-col sm:flex-row sm:justify-center sm:gap-x-6 w-[100%] gap-y-6 px-10">
         <div className="w-full sm:w-1/2 max-w-[600px] flex justify-center">
